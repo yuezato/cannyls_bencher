c47b97f986dd9ba641da21d3a305eb54e033e557
yuuya uezato <yuuya_uezato@dwango.co.jp>
2019-07-31 17:01:34 +0900

    statisticsのための準備

--- src/run_commands.rs
+++ src/run_commands.rs
@@ -1,25 +1,54 @@
-use crate::RealCommand;
+use crate::{Bytes, RealCommand};
 use cannyls::nvm::{FileNvm, NonVolatileMemory};
 use cannyls::storage::{Storage, StorageBuilder};
+use std::collections::BTreeMap;
 use std::ops::Range;
+use std::time::{Duration, Instant};
 
-pub fn do_commands<N>(storage: &mut Storage<N>, commands: &[RealCommand])
+#[derive(PartialOrd, PartialEq, Eq, Ord)]
+enum CommandKind {
+    Put(Bytes),
+
+    Get(Bytes),
+
+    Delete(Bytes),
+
+    DeleteRange,
+}
+
+pub struct History(BTreeMap<CommandKind, Vec<Duration>>);
+
+pub fn do_commands<N>(storage: &mut Storage<N>, commands: &[RealCommand]) -> History
 where
     N: NonVolatileMemory,
 {
+    let mut history: History = History(BTreeMap::new());
+
     for command in commands {
-        do_command(storage, command)
+        do_command(storage, command, &mut history)
     }
+
+    history
 }
 
-pub fn do_command<N>(storage: &mut Storage<N>, command: &RealCommand)
+pub fn do_command<N>(storage: &mut Storage<N>, command: &RealCommand, history: &mut History)
 where
     N: NonVolatileMemory,
 {
     match command {
         RealCommand::Put(lumpid, bytes) => {
             let lump = storage.allocate_lump_data(*bytes).unwrap();
-            let _ = storage.put(&lumpid, &lump).unwrap();
+            {
+                let now = Instant::now();
+                let _ = storage.put(&lumpid, &lump).unwrap();
+                let elapsed = now.elapsed();
+
+                if let Some(v) = history.0.get_mut(&CommandKind::Put(*bytes)) {
+                    v.push(elapsed);
+                } else {
+                    history.0.insert(CommandKind::Put(*bytes), vec![elapsed]);
+                }
+            }
         }
         RealCommand::Get(lumpid) => {
             let _ = storage.get(&lumpid).unwrap();
